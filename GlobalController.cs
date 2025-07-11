019e7f05b9fbb1d7b1a50b72c1109e1b3aa83bb9
Parax342 <42003585+Parax342@users.noreply.github.com>
2025-07-11 11:52:21 -0400

    d

---
+++ GlobalController.cs
@@ -0,0 +1,65 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using System;
+using UnityEngine;
+
+namespace MvL_Unlocked {
+
+	[HarmonyPatch(typeof(NSMB.GlobalController))]
+	public class UnlockValueLimits {
+
+		private static bool hasUnfocusedBefore = false;
+
+		[HarmonyPatch("OnApplicationFocus")]
+		[HarmonyPrefix]
+		static bool OnApplicationFocusPatch(NSMB.GlobalController __instance, ref bool focus, ref Coroutine ___fadeMusicRoutine, ref Coroutine ___fadeSfxRoutine, ref int ___previousFrameRate, ref int ___previousVsyncCount) {
+
+			// This prevents a bug where the fps maxes out if the window is being focused the first time OnApplicationFocus() is run
+			if (!hasUnfocusedBefore) {
+				___previousFrameRate = Application.targetFrameRate;
+				___previousVsyncCount = QualitySettings.vSyncCount;
+				hasUnfocusedBefore = true;
+			}
+
+			// Lower the fps when window is out of focus, if the config is enabled
+			if (Main.LowerFPS.Value) {
+				if (!focus) {
+					// Store old fps and vsync values to restore later
+					___previousFrameRate = Application.targetFrameRate;
+					___previousVsyncCount = QualitySettings.vSyncCount;
+
+					// Lower fps and turn off vsync
+					Application.targetFrameRate = Math.Max(Main.TargetLowerFPS.Value, 1);
+					QualitySettings.vSyncCount = 0;
+				}
+				if (focus) {
+					// Restore previous framerate
+					Application.targetFrameRate = ___previousFrameRate;
+					QualitySettings.vSyncCount = ___previousVsyncCount;
+				}
+			}
+
+			// Copy of vanilla code to un-fade the music when refocusing the window
+			// Exists to seperate music un-fading code from framerate changing Bcode
+			if (focus) {
+				Singleton<NSMB.Settings>.Instance.ApplyVolumeSettings();
+
+				if (___fadeMusicRoutine != null) {
+					__instance.StopCoroutine(___fadeMusicRoutine);
+					___fadeMusicRoutine = null;
+				}
+
+				if (___fadeSfxRoutine != null) {
+					__instance.StopCoroutine(___fadeSfxRoutine);
+					___fadeSfxRoutine = null;
+				}
+
+				return false;
+			}
+
+			return true;
+		}
+	}
+}
---
+++ Main.cs
@@ -0,0 +1,39 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Resources;
+using UnityEngine;
+
+namespace MvL_Unlocked {
+	[BepInPlugin(modID, modName, version)]
+	public class Main : BaseUnityPlugin {
+		private const string modID = "dev.parax.mariovsluigi.fps_fix";
+		private const string modName = "FPS Fix";
+		private const string version = "1.0.0";
+
+		public static Main instance { get; private set; }
+
+		public static BepInEx.Configuration.ConfigFile ModConfig = new BepInEx.Configuration.ConfigFile("fps_fix", true);
+
+		public static ConfigEntry<bool> LowerFPS;
+		public static ConfigEntry<int> TargetLowerFPS;
+
+		void Awake() {
+
+			instance = this;
+
+			Main.LowerFPS = base.Config.Bind<bool>("Stuff", "LowerFPSWhenWindowUnfocused", true, "Lowers the game\'s FPS while the game is out of focus");
+			Main.TargetLowerFPS = base.Config.Bind<int>("Stuff", "TargetUnfocusedFPS", 15, "The framerate the game will be lowered to when not in focus (Number greater than 1)");
+
+			var harmony = new Harmony(modID);
+			harmony.PatchAll();
+
+			Debug.Log("Loaded FPS Fix Succesfully!");
+		}
+	}
+}
